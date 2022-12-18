ba37ff620299ee9e179f07e7c99114473c4e924f
Bob <alexnavin@outlook.com>
2022-12-18 11:55:05 +0600

    navbar, footer, mainpage

--- news/models.py
+++ news/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
+    CATEGORY = (('Science and Technology', 'Science and Technology'),
+                ('Economy', 'Economy'), ('Business', 'Business'),
+                ('Culture', 'Culture'), ('Ideas', 'Ideas'))
+    category = models.CharField(max_length=100, choices=CATEGORY)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='news/images/')
     article = models.TextField()
     date = models.DateTimeField(auto_now=True)
     def __str__(self):
--- newsblog/settings.py
+++ newsblog/settings.py
@@ -21,6 +21,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
