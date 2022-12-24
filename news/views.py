6dd80dc3c3fedac6cdeabb6c718a72f7f6957d6d
Bob <alexnavin@outlook.com>
2022-12-25 00:34:06 +0600

    optimization home view

--- news/views.py
+++ news/views.py
@@ -3,24 +3,12 @@ from .models import Category, News
 
 
 def home(request, pk=0):
-    categories = Category.objects.all()
-
-    def show_news(news):
-        # Если есть новости, показываем, если нет, выводим сообщение
-        context = {'page_name': categories[pk - 1].name if pk else 'News', 'news': news, 'categories': categories}
-        if news:
-            return render(request, template_name='home.html', context=context)
-        else:
-            context['error'] = 'There is no news on this topic.'
-            return render(request, template_name='home.html', context=context)
-
     # Если есть id категории в аргументе, то показываем новости по этой категории
-    if pk:
-        news = News.objects.filter(category_id=pk).order_by('-date')
-        return show_news(news)
-    else:
-        news = News.objects.all().order_by('-date')
-        return show_news(news)
+    categories = Category.objects.all()
+    news = News.objects.filter(category_id=pk).order_by('-date') if pk else News.objects.all().order_by('-date')
+    context = {'page_name': categories[pk - 1].name if pk else 'News', 'news': news,
+               'categories': categories, 'error': 'There is no news on this topic.'}
+    return render(request, template_name='home.html', context=context)
 
 
 def article(request, news_pk):
@@ -32,4 +20,3 @@ def article(request, news_pk):
     except News.DoesNotExist:
         context = {'page_name': 'Does Not Exist', 'categories': categories, 'error': 'This article does not exist'}
         return render(request, template_name='article.html', context=context)
-
