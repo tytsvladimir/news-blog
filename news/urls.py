ac375a9729aaf52ed461864ea8c725d7a057b843
Bob <alexnavin@outlook.com>
2023-01-22 10:39:33 +0600

    def view => class view

--- news/urls.py
+++ news/urls.py
@@ -5,11 +5,11 @@ from .views import *
 urlpatterns = [
     # Manage articles
     path('articles/', ManageArticlesView.as_view(), name='manage_articles'),
-    path('categories/', manage_categories, name='manage_categories'),
+    path('categories/', ManageCategoriesView.as_view(), name='manage_categories'),
     path('new_article/', ArticleCreateView.as_view(), name='new_article'),
     path('edit_article/<int:pk>', ArticleEditView.as_view(), name='edit_article'),
     path('delete_article/<int:pk>', delete_article, name='delete_article'),
-    path('new_category/', new_category, name='new_category'),
-    path('edit_category/<int:pk>', edit_category, name='edit_category'),
+    path('new_category/', CategoryCreateView.as_view(), name='new_category'),
+    path('edit_category/<int:pk>', CategoryEditView.as_view(), name='edit_category'),
     path('delete_category/<int:pk>', delete_category, name='delete_category'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,20 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotFound, HttpResponse
 from django.urls import reverse_lazy
 
-from .services import *
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .forms import *
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
 
 
-#
-# def show_article_list(request, cat_pk=0):
-#     '''Показывает все публикации.
-#     Если передан pk категории, показывает публикации по этой категории'''
-#     context = create_context_for_all(pk=cat_pk)
-#     return render(request, template_name='news/home.html', context=context)
-
 class ArticlesView(ListView):
-    '''Отображение списка всех публикаций'''
+    '''Отображает список всех публикаций'''
     model = Article
     template_name = 'news/home.html'
     context_object_name = 'news'
@@ -34,7 +27,7 @@ class ArticlesView(ListView):
 
 
 class ArticlesCategoryView(ListView):
-    '''Отображение списка публикаций по выбранной категории'''
+    '''Отображает список всех публикаций по выбранной категории'''
     model = Article
     template_name = 'news/home.html'
     context_object_name = 'news'
@@ -52,14 +45,8 @@ class ArticlesCategoryView(ListView):
         return Article.objects.filter(category__slug=self.kwargs['cat_slug'], is_published=True)
 
 
-# def show_article(request, article_slug):
-#     '''Показывает определенную статью'''
-#     context = create_context_for_one(slug=article_slug)
-#     return render(request, template_name='news/article.html', context=context)
-
-
 class ArticleShowView(DetailView):
-    '''Отображение выбранной публикации'''
+    '''Отображает выбранную публикацию'''
     model = Article
     template_name = 'news/article.html'
     slug_url_kwarg = 'article_slug'
@@ -75,13 +62,8 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound(f'<h1>Page not found</h1>\n{exception}')
 
 
-# def manage_articles(request):
-#     articles = Article.objects.filter(author=request.user.id).order_by('-date_of_create')
-#     return render(request, 'news/manage_articles.html', {'articles': articles})
-
-
 class ManageArticlesView(ListView):
-    '''Отображение списка всех публикаций'''
+    '''Отображает список публикаци в личном кабинете'''
     model = Article
     template_name = 'news/manage_articles.html'
     context_object_name = 'articles'
@@ -97,18 +79,8 @@ class ManageArticlesView(ListView):
         return Article.objects.filter(author_id=self.request.user.id)
 
 
-# def create_new_article(request):
-#     '''Показывает форму для создания новой статьи'''
-#     template = 'news/new_article.html'
-#     context = create_context_for_new_article(request=request)
-#     if isinstance(context, dict):
-#         return render(request, template_name=template, context=context)
-#     else:
-#         return redirect('manage_articles')
-
-
 class ArticleCreateView(CreateView):
-    '''Отображение формы для добавления новой публикации'''
+    '''Отображает форму для добавления новой публикации'''
     form_class = ArticleForm
     template_name = 'news/new_article.html'
     success_url = reverse_lazy('manage_articles')
@@ -120,53 +92,53 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
 
-# def edit_article(request, pk):
-#     '''Показывает форму для редактирования существующей статьи'''
-#     template = 'news/edit_article.html'
-#     context = create_context_for_edit_article(request=request, pk=pk)
-#     if isinstance(context, dict):
-#         return render(request, template_name=template, context=context)
-#     else:
-#         return redirect('home')
-
-
 class ArticleEditView(UpdateView):
+    '''Отображает форму для редактирования публикации'''
     form_class = ArticleForm
     model = Article
     template_name = 'news/edit_article.html'
 
 
 def delete_article(request, pk):
+    '''Удаляет публикацию'''
     news = get_object_or_404(Article, pk=pk, author=request.user.id)
     news.delete()
     return redirect('manage_articles')
 
 
-def manage_categories(request):
-    categories = Category.objects.all()
-    return render(request, 'news/manage_categories.html', {'categories': categories})
+class ManageCategoriesView(ListView):
+    '''Отображает список всех категорий в личном кабинете'''
+    model = Category
+    template_name = 'news/manage_categories.html'
+    context_object_name = 'categories'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        page_name = 'Categories'
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = page_name
+        return context
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 
-def new_category(request):
-    template = 'news/new_category.html'
-    context = create_context_for_new_category(request=request)
-    if isinstance(context, dict):
-        return render(request, template_name=template, context=context)
-    else:
-        return redirect('manage_categories')
+class CategoryCreateView(CreateView):
+    '''Отображает форму для добавления новой публикации'''
+    form_class = CategoryForm
+    template_name = 'news/new_category.html'
+    success_url = reverse_lazy('manage_categories')
 
 
-def edit_category(request, pk):
-    '''Показывает форму для редактирования существующей категории'''
-    template = 'news/edit_category.html'
-    context = create_context_for_edit_category(request=request, pk=pk)
-    if isinstance(context, dict):
-        return render(request, template_name=template, context=context)
-    else:
-        return redirect('manage_categories')
+class CategoryEditView(UpdateView):
+    '''Отображает форму для редактирования категории'''
+    form_class = CategoryForm
+    model = Category
+    template_name = 'news/edit_category.html'
 
 
 def delete_category(request, pk):
+    '''Удаляет категорию'''
     category = get_object_or_404(Category, pk=pk)
     category.delete()
     return redirect('manage_categories')
