06383c05ab0f391a7c8b01362f2a0a900ad02efd
Bob <alexnavin@outlook.com>
2023-01-23 20:44:07 +0600

    added cache

--- newsblog/urls.py
+++ newsblog/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from news import views as news_views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', news_views.ArticlesView.as_view(), name='home'),
+    path('', cache_page(60)(news_views.ArticlesView.as_view()), name='home'),
     path('category/<slug:cat_slug>/', news_views.ArticlesCategoryView.as_view(), name='home'),
     path('article/<slug:article_slug>/', news_views.ArticleShowView.as_view(), name='article'),
 
